51b55b8c9659d1b0446565fd09ed38bdcbbadb0a
sibendu <sibendu@users.noreply.github.com>
2018-07-25 19:34:07 +0530

    Update get_ip.py

--- get_ip/get_ip.py
+++ get_ip/get_ip.py
@@ -9,7 +9,7 @@ app = Bottle()
 @app.route('/')
 def get_ip():
     client_ip = request.environ.get('REMOTE_ADDR')
-    return json.dumps({"My client_ip": client_ip})
+    return json.dumps({"Client_ip": client_ip})
 
 
 if __name__ == "__main__":
